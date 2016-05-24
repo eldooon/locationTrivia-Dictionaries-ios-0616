9dc59f299ae916bef9246d923e4b656179d97d84
eldooon <eldonchan91@gmail.com>
2016-05-24 10:01:43 -0400

    done

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -6,14 +6,60 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-
+    
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length;{
+    
+    NSString *lengthLocation = location[@"name"];;
+    
+    lengthLocation = [lengthLocation substringToIndex:MIN(length,[lengthLocation length])];
+    
+    return lengthLocation;
+}
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude{
+    
+    NSMutableDictionary *mDictionary = [[NSMutableDictionary alloc]init];
+    
+    mDictionary[@"name"] = name;
+    mDictionary[@"latitude"] = @(latitude);
+    mDictionary[@"longitude"] = @(longitude);
+    
+    
+    //    [mDictionary setObject:name forKey:@"name"];
+    //    [mDictionary setObject:[NSNumber numberWithFloat:latitude] forKey:@"latitude"];
+    //    [mDictionary setObject:[NSNumber numberWithFloat:longitude] forKey:@"longitude"];
+    
+    return mDictionary;
+}
+-(NSArray *)namesOfLocations:(NSArray *)locations{
+    
+    NSMutableArray *mLocation = [locations valueForKey:@"name"];
+    
+    return mLocation;
+}
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary{
+    
+    BOOL valid = NO;
+    
+    if ([dictionary objectForKey:@"name"] && [dictionary objectForKey:@"latitude"] && [dictionary objectForKey:@"longitude"] && [[dictionary allKeys]count] == 3){
+        
+        valid = YES;
+    }
+    
+    return valid;
+}
+-(NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations{
+    
+    for (NSDictionary *key in locations){
+        if ([key[@"name"] isEqual: name]){
+            
+            return key;
+        }
+    }
+    
+    return nil;
+}
 
 @end
